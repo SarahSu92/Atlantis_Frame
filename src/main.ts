5e3cc55e2bd92470d45926ea41d3082e876fd17e
SarahSu92 <sarah.sundberg@outlook.com>
2025-04-24 22:27:49 +0200

    (feat): added nav menu on max width 1040

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './style.css'
+
+//Navigation menu
+const menubtn = document.querySelector('.menubtn');
+const closebtn = document.querySelector('.closebtn');
+
+menubtn?.addEventListener("click", () => {
+    menubtn.classList.toggle("active");
+    closebtn.classList.toggle("active");
+});
