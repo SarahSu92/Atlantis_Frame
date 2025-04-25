40815e2fbd4aacaad31decb76dd2369f951b2f80
SarahSu92 <sarah.sundberg@outlook.com>
2025-04-25 10:55:20 +0200

    (feat): finished menu for tablet and mobile

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import './style.css'
 
 //Navigation menu
-const menubtn = document.querySelector('.menubtn');
-const closebtn = document.querySelector('.closebtn');
+const menubtn = document.querySelector('.menubtn') as HTMLElement | null;
+const nav = document.querySelector('.nav')as HTMLElement | null;
 
-menubtn?.addEventListener("click", () => {
-    menubtn.classList.toggle("active");
-    closebtn.classList.toggle("active");
-});
+if (menubtn && nav) {
+    menubtn.addEventListener("click", () => {
+        menubtn.classList.toggle("active");
+        nav.classList.toggle("active");
+    });
+}
+
+
