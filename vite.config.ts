5846eecd5a869191f10e925d20f0ffcf6356801a
SarahSu92 <sarah.sundberg@outlook.com>
2025-05-02 20:32:39 +0200

    (build):adjusted vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Atlantis_Frame/', // replace with your actual repo name
+  'base': '/Atlantis_Frame/', // replace with your actual repo name
 });
