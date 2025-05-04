99837822a091d58ea80d3a5de988db9a483edb10
SarahSu92 <sarah.sundberg@outlook.com>
2025-05-04 13:40:04 +0200

    (test)

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './style.css'
+
+console.log('hej');
+
+//Navigation menu
+const menubtn = document.querySelector('.menubtn');
+const nav = document.querySelector('.nav');
+
+if (menubtn && nav) {
+    menubtn.addEventListener("click", () => {
+        menubtn.classList.toggle("active");
+        nav.classList.toggle("active");
+    });
+}
+
+
+// Example articles below hero section
+const article = document.querySelector('#example');
+article.innerHTML += `
+<div> Hello wolrd</div>
+<div> Hello</div>
+`;
